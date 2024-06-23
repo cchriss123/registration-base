cbc4cf73045d975a6a71407fc4744480d62dc9d1
Christofer Nyström <christofer.nystrom@iths.se>
2024-06-23 19:00:21 +0200

    adds query to insert syslog

---
+++ src/database/syslogDb.ts
@@ -0,0 +1,21 @@
+import { poolDb } from './poolDb';
+
+export interface Syslog {
+    entity_type: string;
+    action: string;
+    description?: string;
+    details?: string;
+    entity_id: string;
+    title: string;
+    created_by?: string;
+
+}
+
+export async function insertSyslog(syslog: Syslog) {
+
+        const keys = Object.keys(syslog) as string[];
+        const placeholders = keys.map(() => '?').join(', ');
+        const values = keys.map(key => syslog[key]);
+        const query = `INSERT INTO SYSTEM_LOG (${keys.join(', ')}) VALUES (${placeholders})`;
+        await poolDb.execute(query, values);
+}
