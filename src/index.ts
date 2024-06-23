6052fa6213b8cad3946c00a8d1c86970fb81dd89
Christofer Nyström <christofer.nystrom@iths.se>
2024-06-23 12:56:17 +0200

    empty ts server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 8097;
+
+app.use(cors());
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+app.get('/api/ping', (_req: Request, res: Response) => res.send('pong'));
+
+
+
