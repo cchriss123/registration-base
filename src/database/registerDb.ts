e079377d6594212460fdc1a3f7aae9cd3811ad2a
Christofer Nyström <christofer.nystrom@iths.se>
2024-06-23 19:09:17 +0200

    adds bcrypt and fixes syntax in insertSuperAdmin

--- src/database/registerDb.ts
+++ src/database/registerDb.ts
@@ -1,11 +1,14 @@
 import { insertSyslog } from "./syslogDb";
 import {poolDb} from "./poolDb";
 import {RowDataPacket} from "mysql2";
+import bcrypt from "bcrypt";
 
 export async function insertSuperAdmin() {
 
-    const rows = await poolDb.query<RowDataPacket[]>('SELECT * FROM USER WHERE user_type = "super_admin"');
+    const [rows] = await poolDb.query<RowDataPacket[]>('SELECT * FROM USER WHERE user_type = "super_admin"');
     if (rows.length !== 0) return;
+
+    const hashedPassword = await bcrypt.hash('super_admin', 10);
 
 
 
