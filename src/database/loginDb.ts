51abc6098a0280f1b4c977a5dcd28b2739981172
Christofer Nyström <christofer.nystrom@iths.se>
2024-06-30 19:40:01 +0200

    Adds saveRefreshToken implementation

--- src/database/loginDb.ts
+++ src/database/loginDb.ts
@@ -1,5 +1,19 @@
-export async function saveRefreshToken(refreshToken: string, id: never) {
-    //TODO
+import { poolDb } from './poolDb';
+import * as syslogDb from './syslogDb';
+import {Syslog} from "./syslogDb";
+
+export async function saveRefreshToken(refreshToken: string, id: string) : Promise<void> {
+    await poolDb.execute('UPDATE USER SET refresh_token = ? WHERE id = ?', [refreshToken, id]);
+
+    const syslog: Syslog = {
+        entity_id: id,
+        entity_type: 'USER',
+        action: 'UPDATE',
+        title: 'Refresh Token Update',
+        description: 'Someone logged in and a new refresh token was generated.',
+        created_by: id
+    }
+    await syslogDb.insertSyslog(syslog);
 }
 
 export async function selectIdByEmail(email: string) {
