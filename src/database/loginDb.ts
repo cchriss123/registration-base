2b19865d2ca42ffc49846c956fbd0dae7db537e2
Christofer Nyström <christofer.nystrom@iths.se>
2024-06-30 19:44:32 +0200

    Adds selectUserPasswordByEmail implementation

--- src/database/loginDb.ts
+++ src/database/loginDb.ts
@@ -1,6 +1,7 @@
 import { poolDb } from './poolDb';
 import * as syslogDb from './syslogDb';
 import {Syslog} from "./syslogDb";
+import {RowDataPacket} from "mysql2";
 
 export async function saveRefreshToken(refreshToken: string, id: string) : Promise<void> {
     await poolDb.execute('UPDATE USER SET refresh_token = ? WHERE id = ?', [refreshToken, id]);
@@ -20,6 +21,8 @@ export async function selectIdByEmail(email: string) {
     //TODO
 }
 
-export async function selectUserPasswordByEmail(email: string) {
-    //TODO
+export async function selectUserPasswordByEmail(email: string) : Promise<string | null> {
+    const query = 'SELECT password FROM USER WHERE email = ? AND is_deleted = false';
+    const [rows] = await poolDb.query<RowDataPacket[]>(query, [email]);
+    return rows[0].password || null;
 }
